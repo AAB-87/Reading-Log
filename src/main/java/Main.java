f9c5996add3d404c74fab49063ca374d4f4c15df
AAB-87 <andrej-abrekov@yandex.ru>
2023-06-02 20:01:23 +0400

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,17 @@ public class Main {
   public static void main(String[] args) {
     BonusService service = new BonusService();
 
+    long expected = 30;
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+    long actual = service.calculate(amount, registered);
+    // производим проверку (сравниваем ожидаемый и фактический):
+    // если true — то PASS
+    // если false — то FAIL
+    boolean passed = expected == actual;
+    // выводим результат
+    System.out.println(passed);
   }
 }
 
